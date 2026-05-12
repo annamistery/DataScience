d7fe0ffd015b5a46575b5a1a6597325006024f3a
Анна <147938295+annamistery@users.noreply.github.com>
2026-05-12 12:56:51 +0300

    Update image display to use container width

--- app_factories.py
+++ app_factories.py
@@ -23,7 +23,7 @@ st.header("Приложение по подбору фабрик")
 image_url = 'https://github.com/annamistery/DataScience/blob/main/image_legprom.jpg'
 
 # Display the image using the URL
-st.image(image_url, caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+st.image(image_url, caption=None, width=None, use_container_width=True, clamp=False, channels="RGB", output_format="auto")
 
 st.subheader("Введите необходимые спецификации вашего заказа")
 
